4dfff345bcf911a7d7aada677073c0e49a297f2c
ManoharPulluru <125332698+ManoharPulluru@users.noreply.github.com>
2023-06-04 19:51:56 +0530

    changing css and db

--- src/components/PostViewComponents/Header.js
+++ src/components/PostViewComponents/Header.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import '../../stylecss/postviewHeader.css'
-import logo from '../../images/icon.png'
-import camera from '../../images/camera.png'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import '../../stylecss/postviewHeader.css';
+import logo from '../../images/icon.png';
+import camera from '../../images/camera.png';
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
     <div className='containerheader'>
-      <div className='boxheader'><img className='logo' alt='logo' src={logo}/></div>
-      <div className='instacloneText boxheader'>Instaclone</div>
-      <div className='boxheader'>
+      <div className='logo-container'>
+        <img className='logo' alt='logo' src={logo} />
+      </div>
+      <div className='instacloneText'>Instaclone</div>
+      <div className='camera-container'>
         <Link to='/form'>
-        <img className='camera' alt='camera' src={camera}/></Link></div>
+          <img className='camera' alt='camera' src={camera} />
+        </Link>
+      </div>
     </div>
-  )
-}
-export default Header
+  );
+};
+
+export default Header;
